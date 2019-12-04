6e4bf051d204d1bd5fe636971f48add638d67993
Dmitrij <dmitrij.titov@gmail.com>
2019-12-04 08:09:10 +0200

    #initial commit

---
+++ src/main/java/com/oddschecker/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oddschecker.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
